aa2b6be15c396b7d899505617f30b3c76fd77659
Orienta <orie.chukwunonso>
2020-02-27 10:29:08 +0100

    Updated script.js file

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-$("#searchTerm").keypress(function (e) {
-  if (e.keyCode === 13) {
+$("#searchTerm").keypress(function(e){
+  if(e.keyCode === 13){
     var searchTerm = $("#searchTerm").val();
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-    $.ajax({
-      url: url,
-      type: "GET",
-      async: false,
-      dataType: "json",
-      success: function (data, status, jqXHR) {
-        console.log(data);
-        for (var i = 0; i < data[1].length; i++) {
-          $("#output").prepend("<div><div class='well'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-
-        }
-      } });
-
-
-  }
-});
-
-
-
-
-
-$("#search").on("click", function () {
-  var searchTerm = $("#searchTerm").val();
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
   $.ajax({
     url: url,
     type: "GET",
     async: false,
     dataType: "json",
-    success: function (data, status, jqXHR) {
+    success: function(data, status, jqXHR){
       // console.log(data);
-      for (var i = 0; i < data[1].length; i++) {
-        $("#output").prepend("<div><div class='well'><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-
+      for(var i = 0; i < data[1].length; i++) {
+        $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+        
       }
-    } });
+    }
+    
+  })
+  }
+})
 
 
 
 
-      var scope= https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=shoes&utf8=&format=json
+
+$("#search").on("click", function(){
+  var searchTerm = $("#searchTerm").val();
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+  $.ajax({
+    url: url,
+    type: "GET",
+    async: false,
+    dataType: "json",
+    success: function(data, status, jqXHR){
+      // console.log(data);
+      for(var i = 0; i < data[1].length; i++) {
+        $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+        
+      }
+    }
+    
+  })
+  
+})
